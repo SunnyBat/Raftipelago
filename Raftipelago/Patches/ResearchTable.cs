410fedebc3640b59c3558e213d48bde8c8da696d
SunnyBat <Sunnybat@yahoo.com>
2021-12-02 21:40:27 -0800

    Initial commit

---
+++ Raftipelago/Patches/ResearchTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace Raftipelago.Patches
+{
+    [HarmonyPatch(typeof(ResearchTable))]
+    public class HarmonyPatch_ResearchTable
+    {
+    }
+}
---
+++ Raftipelago/Raftipelago.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Raftipelago : Mod
+{
+    public void Start()
+    {
+        Debug.Log("Mod Raftipelago has been loaded!");
+    }
+
+    public void OnModUnload()
+    {
+        Debug.Log("Mod Raftipelago has been unloaded!");
+    }
+}
