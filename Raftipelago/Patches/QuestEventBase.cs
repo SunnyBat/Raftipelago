9a61b872a068984e5a4e50073c64f02a9524f877
SunnyBat <Sunnybat@yahoo.com>
2021-12-26 03:08:08 -0800

    Forgot to remove some test code oops

--- Raftipelago/Patches/QuestEventBase.cs
+++ Raftipelago/Patches/QuestEventBase.cs
@@ -46,45 +46,9 @@ namespace Raftipelago.Patches
 				ComponentManager<IArchipelagoLink>.Value.LocationUnlocked(pickupName);
 				(ComponentManager<NotificationManager>.Value.ShowNotification("Research") as Notification_Research)
 					.researchInfoQue.Enqueue(new Notification_Research_Info(pickupName, localSteamId, ComponentManager<SpriteManager>.Value.GetArchipelagoSprite()));
+				return false;
 			}
-			else
-			{
-				Debug.Log("Interact: " + __instance.name + " (" + successFull + ")");
-				try
-				{
-					foreach (var obj in __instance.GetComponents<object>())
-					{
-						Debug.Log("TYPE: " + obj.GetType());
-						if (obj.GetType() == typeof(QuestInteractableComponent_ObjectOnOff))
-						{
-							var dataComponents = (List<QuestInteractable_ComponentData_ObjectOnOff>)obj.GetType().GetField("dataComponents").GetValue(obj);
-							foreach (var dataComp in dataComponents)
-							{
-								foreach (var gameObject in dataComp.gameObjects)
-								{
-									gameObject.SetActiveSafe(false);
-								}
-							}
-						}
-						else
-						{
-							foreach (var f in obj.GetType().GetRuntimeFields())
-							{
-								Debug.Log($"{f.Name}:" + f.GetValue(obj));
-							}
-							foreach (var p in obj.GetType().GetRuntimeProperties())
-							{
-								Debug.Log($"{p.Name}:" + p.GetValue(obj));
-							}
-						}
-					}
-				}
-				catch (Exception e)
-				{
-					Debug.Log(e);
-				}
-			}
-            return false;
+            return true;
 		}
 	}
 }
