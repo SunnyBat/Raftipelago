28ff267520f101844f623befde1b4c9a704e1889
SunnyBat <Sunnybat@yahoo.com>
2022-01-14 22:48:08 -0800

    Dedupe chat messages in multiplayer

--- Raftipelago/Patches/ChatManager.cs
+++ Raftipelago/Patches/ChatManager.cs
@@ -18,11 +18,7 @@ namespace Raftipelago.Patches
 			{
 				if (Semih_Network.IsHost)
 				{
-					if (textWriterSteamID.m_SteamID == __instance.network?.LocalSteamID.m_SteamID) // Local player (host) sending a message
-					{
-						ComponentManager<IArchipelagoLink>.Value.SendChatMessage(text);
-					}
-					else if (CommonUtils.TryGetArchipelagoPlayerIdFromSteamId(textWriterSteamID.m_SteamID, out int playerId)) // Archipelago sending a message
+					if (CommonUtils.TryGetArchipelagoPlayerIdFromSteamId(textWriterSteamID.m_SteamID, out int playerId)) // Archipelago sending a message
 					{
 						Message_IngameChat message = new Message_IngameChat(Messages.Ingame_Chat_Message, __instance, textWriterSteamID, text);
 						ComponentManager<Semih_Network>.Value.RPC(message, Target.Other, EP2PSend.k_EP2PSendReliable, NetworkChannel.Channel_Game);
@@ -33,12 +29,38 @@ namespace Raftipelago.Patches
 						ComponentManager<IArchipelagoLink>.Value.SendChatMessage($"(Local Player {SteamFriends.GetFriendPersonaName(textWriterSteamID)}): {text}");
 					}
 				}
-				else
+				else if (CommonUtils.TryGetArchipelagoPlayerIdFromSteamId(textWriterSteamID.m_SteamID, out int playerId)) // Only send Archipelago messages in chat
 				{
-					__instance.chatFieldController.AddUITextMessage(text, textWriterSteamID); // Always just write it
+					__instance.chatFieldController.AddUITextMessage(text, textWriterSteamID);
 				}
 			}
 			typeof(ChatManager).GetMethod("SendRecieveChatMessageAction", BindingFlags.Instance | BindingFlags.NonPublic).Invoke(__instance, null);
+			return false;
+		}
+	}
+
+	[HarmonyPatch(typeof(ChatManager), "SendChatMessage", typeof(string), typeof(CSteamID))]
+	public class HarmonyPatch_ChatManager_SendChatMessage
+	{
+		[HarmonyPrefix]
+		public static bool AlwaysReplace(string p_message, CSteamID p_steamID,
+			Semih_Network ___network,
+			ChatManager __instance)
+		{
+			if (___network == null)
+			{
+				return false;
+			}
+
+			Message_IngameChat message = new Message_IngameChat(Messages.Ingame_Chat_Message, __instance, p_steamID, p_message);
+			if (Semih_Network.IsHost)
+			{
+				ComponentManager<IArchipelagoLink>.Value.SendChatMessage(p_message);
+			}
+			else
+			{
+				___network.SendP2P(___network.HostID, message, EP2PSend.k_EP2PSendReliable, NetworkChannel.Channel_Game);
+			}
 			return false;
 		}
 	}
